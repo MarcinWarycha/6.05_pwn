b016d4220773324d500893a20a17bc11d7ebb59b
MarcinWarycha <45559627+MarcinWarycha@users.noreply.github.com>
2019-10-19 13:36:41 +0200

    Add files via upload

---
+++ sql procedure.sql
@@ -0,0 +1,38 @@
+# -------------------------------------------------------------------------------------
+# Functions & procedures
+# -------------------------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS reprezentacja;
+
+DELIMITER //
+CREATE PROCEDURE reprezentacja (IN nazwa VARCHAR(20))
+BEGIN
+
+  SET @liczba        = (SELECT count(*) AS liczba FROM zawodnicy group by nazwa);
+  SET @sredni_wzrost = (SELECT avg(wzrost) FROM zawodnicy WHERE kraj = nazwa);
+  
+  #[to do solve compile error]
+  BEGIN
+	SELECT imie zawodnicy WHERE kraj = nazwa; 
+  END;
+  
+   #[to do solve compile error]
+  BEGIN
+	SELECT id_skoczka FROM zawodnicy kraj = nazwa;
+  END;
+  
+  BEGIN
+	SELECT t.nazwisko_t as nazwisko_trenera from zawodnicy z join trenerzy t on (z.kraj = t.kraj) WHERE z.kraj = nazwa limit 1;
+  END;
+  
+  BEGIN
+	SELECT @liczba AS l_reprezentantow, @sredni_wzrost as s_wzrost;
+  END;
+  
+END //
+DELIMITER ;
+
+#[to do solve compile error]
+reprezentacja("POL"); 
+
+#[to do remove this line and insert your name]
